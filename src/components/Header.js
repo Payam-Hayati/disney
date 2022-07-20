c6050a5a0e0ff85646d8707f1ff79297ae3649d2
Your Name <payam.hayati@yahoo.com>
2022-07-20 20:09:09 +0430

    step one

---
+++ src/components/Header.js
@@ -0,0 +1,93 @@
+import React from "react";
+import styled from "styled-components";
+
+const Header = () => {
+  return (
+    <Nav>
+      <Logo src="images/logo.svg" alt="logo" />
+      <NavMenu>
+        <a href="/">
+          <img src="images/home-icon.svg" alt="" />
+          <span>HOME</span>
+        </a>
+
+        <a href="/">
+          <img src="images/search-icon.svg" alt="" />
+          <span>SEARCH</span>
+        </a>
+
+        <a href="/">
+          <img src="images/watchlist-icon.svg" alt="" />
+          <span>WATCHLIST</span>
+        </a>
+
+        <a href="/">
+          <img src="images/original-icon.svg" alt="" />
+          <span>ORIGINALS</span>
+        </a>
+
+        <a href="/">
+          <img src="images/movie-icon.svg" alt="" />
+          <span>MOVIES</span>
+        </a>
+
+        <a href="/">
+          <img src="images/series-icon.svg" alt="" />
+          <span>SERIES</span>
+        </a>
+      </NavMenu>
+
+      <UserImg src="https://avatars.githubusercontent.com/u/89186898?v=4" />
+    </Nav>
+  );
+};
+
+export default Header;
+
+const Nav = styled.nav`
+  height: 70px;
+  background: #090b13;
+  display: flex;
+  align-items: center;
+  padding: 0 360;
+`;
+
+const Logo = styled.img`
+  width: 80px;
+`;
+
+const NavMenu = styled.div`
+  display: flex;
+  flex: 1;
+  margin-left: 25px;
+  align-items: center;
+
+  a {
+    display: flex;
+    align-items: center;
+    padding: 0 12px;
+    cursor: pointer;
+
+    img {
+      height: 20px;
+    }
+
+    span {
+      font-size: 13px;
+      letter-spacing: 1.42px;
+      transition: all 1s ease-in-out;
+      -webkit-transition: all 1s ease-in-out;
+
+      &:hover {
+        border-bottom: 1px solid white;
+      }
+    }
+  }
+`;
+
+const UserImg = styled.img`
+  width: 48px;
+  height: 48px;
+  border-radius: 50%;
+  cursor: pointer;
+`;

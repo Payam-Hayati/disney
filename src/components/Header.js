c22ea499a928b88fa58bb2aedb700b1ddff50ad6
Your Name <payam.hayati@yahoo.com>
2022-07-21 21:35:18 +0430

    Add: Viewvers

--- src/components/Header.js
+++ src/components/Header.js
@@ -50,6 +50,7 @@ const Nav = styled.nav`
   display: flex;
   align-items: center;
   padding: 0 360;
+  overflow-x:hidden;
 `;
 
 const Logo = styled.img`
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import ImgSlider from "./ImgSlider";
+import Viewers from "./Viewers";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <Container>
+      <ImgSlider />
+      <Viewers />
+    </Container>
+  );
+};
 
-export default Home
+export default Home;
+
+const Container = styled.main`
+  min-height: calc(100vh - 70px);
+  padding: 0 calc(3.5vw + 5px);
+  background-image: url("/images/home-background.png");
+  background-position: center; /* Center the image */
+  background-repeat: no-repeat; /* Do not repeat the image */
+  background-size: cover;
+  background-attachment: fixed;
+  overflow:hidden;
+`;
